b99a613d53a58782631cd40b93ab198f04da38c9
Santiago Cárdenas <santiago_cardenas_r@outlook.com>
2019-10-23 12:25:09 -0500

    Easy navigation between fields

--- Assets/Scripts/Submit.cs
+++ Assets/Scripts/Submit.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using GoogleSheetsToUnity;
+using UnityEngine.EventSystems;
 public class Submit : MonoBehaviour
 {
     public InputField _name;
@@ -18,6 +19,9 @@ public class Submit : MonoBehaviour
     private string associatedSheet = "1GrsTxAEOe1NIzwune7jvqwHKf6SpIp4ZrB5vsVqxwSs";
     private string associatedWorkSheet = "Data";
 
+
+    
+
     void Start()
     {
         //_name = gameObject.GetComponent<InputField>();
@@ -28,6 +32,57 @@ public class Submit : MonoBehaviour
         lastName_field = "";
         mail_field = "";
         enterprise_field = "";
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (EventSystem.current != null)
+            {
+                GameObject selected = EventSystem.current.currentSelectedGameObject;
+                if (selected == null)
+                {
+                    Selectable found = (Selectable.allSelectablesArray.Length > 0) ? Selectable.allSelectablesArray[0] : null;
+
+                    if (found != null)
+                    {
+                        //simple reference so that selected isn't null and will proceed
+                        //past the next if statement
+                        selected = found.gameObject;
+                    }
+                }
+            if (selected != null)
+            {
+                Selectable current = (Selectable)selected.GetComponent("Selectable");
+
+                if (current != null)
+                {
+                    Selectable nextDown = current.FindSelectableOnDown();
+                    Selectable nextUp = current.FindSelectableOnUp();
+                    Selectable nextRight = current.FindSelectableOnRight();
+                    Selectable nextLeft = current.FindSelectableOnLeft();
+
+                    if (nextDown != null)
+                    {
+                        nextDown.Select();
+                    }
+                    else if (nextRight != null)
+                    {
+                        nextRight.Select();
+                    }
+                    else if (nextUp != null)
+                    {
+                        nextUp.Select();
+                    }
+                    else if (nextLeft != null)
+                    {
+                        nextLeft.Select();
+                    }
+                }
+            }
+        }
+    }
     }
 
     public void submit_information()
