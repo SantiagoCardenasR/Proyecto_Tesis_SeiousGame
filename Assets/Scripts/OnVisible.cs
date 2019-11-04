c6bb78da85d3748e84e95da3440fd37622188216
Santiago Cárdenas <santiago_cardenas_r@outlook.com>
2019-11-04 16:45:08 -0500

    New_Assests

---
+++ Assets/Scripts/OnVisible.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class OnVisible : MonoBehaviour
+{
+    public Canvas canvas;
+    private void OnBecameVisible()
+    {
+        canvas.enabled = true;
+    }
+
+    private void OnBecameInvisible()
+    {
+        canvas.enabled = false;
+    }
+}
